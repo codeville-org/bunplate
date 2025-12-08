e4499068c15b9c9312809e018da6357b814c73b2
iamVihanga <vihangarashansilva@gmail.com>
2025-12-08 23:48:01 +0530

    fix: api documentation error in vercel - test 2

--- apps/api/src/lib/open-api-config.ts
+++ apps/api/src/lib/open-api-config.ts
@@ -7,7 +7,7 @@ import { BASE_PATH } from "./constants";
 
 export default function configureOpenAPI(app: OpenAPI): void {
   app.doc31("/doc", {
-    openapi: "3.0.0",
+    openapi: "3.1.0",
     info: {
       version: packageJson.version,
       title: "Bunplate (by CodeVille)"
